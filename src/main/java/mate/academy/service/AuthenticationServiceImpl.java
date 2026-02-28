489e00a0bb4f8c20a3447e1487b040f75e1dcea5
Dima Babenko <klapster1@gmail.com>
2026-02-28 19:57:21 +0300

    Todo comments removed and fixed logger masseges

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -9,8 +9,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
     @Override
     public User login(String login, String password) throws AuthenticationException {
-        //TODO: add corresponding log message about method login was called
-        logger.info("Login method was called. Params: loggin = {}", login);
+        logger.info("Login method was called. Params: login = {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
